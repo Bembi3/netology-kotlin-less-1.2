af3089328b7de30c08dbc8fb6581d793cebe289e
Alsou <5416037@mail.ru>
2021-07-31 14:34:26 +0300

    init

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val likes = 51
+    if (likes % 2 == 0){
+        println ("людям")
+    }else {
+        println ("человеку")
+    }
+   // println("Hello World!")
+}
